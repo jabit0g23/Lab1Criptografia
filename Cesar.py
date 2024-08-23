f3e54fd4b1800247c730526e4344fdc8c9cb8099
jabit0g23 <jabit0g23@gmail.com>
2024-08-23 10:37:45 -0400

    Algoritmo de cifrado Cesar

---
+++ Cesar.py
@@ -0,0 +1,23 @@
+def cifrado_cesar(texto, desplazamiento):
+    resultado = ""
+    for caracter in texto:
+        if caracter.isalpha():
+            ascii_offset = 65 if caracter.isupper() else 97
+            nueva_letra = chr((ord(caracter) - ascii_offset + desplazamiento) % 26 + ascii_offset)
+            resultado += nueva_letra
+        elif caracter.isdigit():
+            nueva_cifra = chr((ord(caracter) - ord('0') + desplazamiento) % 10 + ord('0'))
+            resultado += nueva_cifra
+        else:
+            resultado += caracter
+    return resultado
+
+# Ejemplo de uso
+texto_original = "criptografia y seguridad en redes 2024"
+desplazamiento = 9
+texto_cifrado = cifrado_cesar(texto_original, desplazamiento)
+print("Texto cifrado:", texto_cifrado)
+
+# Para descifrar
+texto_descifrado = cifrado_cesar(texto_cifrado, -desplazamiento)
+print("Texto descifrado:", texto_descifrado)
